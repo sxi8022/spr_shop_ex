9fb5feabf298de4421df376f29b36611f9d5cfe6
sxi8022 <sxi8022@gmail.com>
2023-06-27 10:24:49 +0900

    폴더별 관심상품 조회 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.sparta.myselectshop.repository;
 
+import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.entity.User;
 import org.springframework.data.domain.Page;
@@ -10,4 +11,8 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByUser(User user, Pageable pageable);
+
+    // 현재 로그인한 유저가 등록한 폴더의 상품을 조회
+    // 반환형은 Product이어야한 dto안됨
+    Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
 }
--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -102,6 +102,18 @@ public class ProductService {
 
         productFolderRepository.save(new ProductFolder(product, folder));
     }
+    @Transactional(readOnly = true)
+    public Page<ProductResponseDto> getProductsInFolder(Long folderId, int page, int size, String sortBy, boolean isAsc, User user) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        Page<Product> productList = productRepository.findAllByUserAndProductFolderList_FolderId(user, folderId, pageable);
+
+        Page<ProductResponseDto> responseDtoList = productList.map(ProductResponseDto::new);
+
+        return responseDtoList;
+    }
 
 /*    public List<ProductResponseDto> getAllProducts() {
         List<Product> productList = productRepository.findAll();
