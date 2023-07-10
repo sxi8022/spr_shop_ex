bebe7331392e3987642d5106f53413dc34d91f90
sxi8022 <sxi8022@gmail.com>
2023-07-10 17:40:13 +0900

    -컨트롤러 mock 테스트

--- src/main/java/com/sparta/myselectshop/MySelectShopApplication.java
+++ src/main/java/com/sparta/myselectshop/MySelectShopApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-@EnableJpaAuditing
+// @EnableJpaAuditing
 public class MySelectShopApplication {
 
     public static void main(String[] args) {
